86d9a91fb4e7132a10063e4cd173fd3d89d2c358
rishabh <rjain23112002@gmail.com>
2025-02-23 10:33:23 +0530

    color theme

--- lua/plugins/java/init.lua
+++ lua/plugins/java/init.lua
@@ -1,36 +1,36 @@
 return {
-  "nvim-java/nvim-java",
+  'nvim-java/nvim-java',
   config = false,
   lazy = true,
   dependencies = {
     {
-      "neovim/nvim-lspconfig",
+      'neovim/nvim-lspconfig',
       opts = {
         setup = {
           jdtls = function()
             -- Your nvim-java configuration goes here
-            require("java").setup({
+            require('java').setup {
               root_markers = {
-                "settings.gradle",
-                "pom.xml",
-                "build.gradle",
-                "mvnw",
-                "gradlew",
-                "build.gradle",
+                'settings.gradle',
+                'pom.xml',
+                'build.gradle',
+                'mvnw',
+                'gradlew',
+                'build.gradle',
               },
               -- load java debugger plugins
               java_debug_adapter = {
-                enable = false,
+                enable = true,
               },
               notifications = {
                 -- enable 'Configuring DAP' & 'DAP configured' messages on start up
-                dap = false,
+                dap = true,
               },
               -- load java test plugins
               java_test = {
                 enable = false,
               },
-            })
+            }
           end,
         },
       },
