8242db20fdbecc773e342f5f4835a0b5fc29ed49
Rishabh Jain <rjain23112002@gmail.com>
2025-05-08 20:38:59 +0530

    color scheme bg transparent

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,33 @@
 return {
+  -- Configure LazyVim to load tokyonight
   {
     'folke/tokyonight.nvim',
+    lazy = true,
     priority = 1000,
+    enabled = true,
     opts = { style = 'night' },
-
+  },
+  {
+    'rebelot/kanagawa.nvim',
+    lazy = false,
+    priority = 1000,
+    enabled = false,
     config = function()
-      -- load the colorscheme here
-      vim.cmd [[colorscheme tokyonight]]
+      vim.cmd 'colorscheme kanagawa-wave'
+    end,
+  },
+  {
+    'EdenEast/nightfox.nvim',
+    priority = 1000,
+    enabled = true,
+    config = function()
+      require('nightfox').setup {
+
+        options = {
+          transparent = true,
+        },
+      }
+      vim.cmd 'colorscheme carbonfox'
     end,
   },
 }
