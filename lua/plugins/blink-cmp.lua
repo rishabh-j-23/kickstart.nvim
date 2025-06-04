587740fc4cfd01bcd2d309d337a1baddaf477d3f
Rishabh Jain <rjain23112002@gmail.com>
2025-06-04 22:55:07 +0530

    feat: switched to blink.cmp

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -1,13 +1,11 @@
 return { -- Autocompletion
   'saghen/blink.cmp',
   event = 'VimEnter',
-  version = '1.*',
 
   dependencies = {
     -- Snippet Engine
     {
       'L3MON4D3/LuaSnip',
-      version = '2.*',
       build = (function()
         -- Build Step is needed for regex support in snippets.
         -- This step is not supported in many windows environments.
@@ -61,8 +59,61 @@ return { -- Autocompletion
 
       -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
       --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
+      ['<A-1>'] = {
+        function(cmp)
+          cmp.accept { index = 1 }
+        end,
+      },
+      ['<A-2>'] = {
+        function(cmp)
+          cmp.accept { index = 2 }
+        end,
+      },
+      ['<A-3>'] = {
+        function(cmp)
+          cmp.accept { index = 3 }
+        end,
+      },
+      ['<A-4>'] = {
+        function(cmp)
+          cmp.accept { index = 4 }
+        end,
+      },
+      ['<A-5>'] = {
+        function(cmp)
+          cmp.accept { index = 5 }
+        end,
+      },
+      ['<A-6>'] = {
+        function(cmp)
+          cmp.accept { index = 6 }
+        end,
+      },
+      ['<A-7>'] = {
+        function(cmp)
+          cmp.accept { index = 7 }
+        end,
+      },
+      ['<A-8>'] = {
+        function(cmp)
+          cmp.accept { index = 8 }
+        end,
+      },
+      ['<A-9>'] = {
+        function(cmp)
+          cmp.accept { index = 9 }
+        end,
+      },
+      ['<A-0>'] = {
+        function(cmp)
+          cmp.accept { index = 10 }
+        end,
+      },
     },
-
+    cmdline = {
+      keymap = { preset = 'inherit' },
+      completion = { menu = { auto_show = true } },
+    },
     appearance = {
       -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
       -- Adjusts spacing to ensure icons are aligned
@@ -73,6 +124,19 @@ return { -- Autocompletion
       -- By default, you may press `<c-space>` to show the documentation.
       -- Optionally, set `auto_show = true` to show the documentation after a delay.
       documentation = { auto_show = false, auto_show_delay_ms = 500 },
+      menu = {
+        draw = {
+          columns = { { 'item_idx' }, { 'kind_icon' }, { 'label', 'label_description', gap = 1 } },
+          components = {
+            item_idx = {
+              text = function(ctx)
+                return ctx.idx == 10 and '0' or ctx.idx >= 10 and ' ' or tostring(ctx.idx)
+              end,
+              highlight = 'BlinkCmpItemIdx', -- optional, only if you want to change its color
+            },
+          },
+        },
+      },
     },
 
     sources = {
